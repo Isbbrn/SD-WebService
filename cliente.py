9eea661a6fe36284befe062efc48072efc240810
Isbbrn <30944679+Isbbrn@users.noreply.github.com>
2023-04-26 23:03:11 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,12 +1,27 @@
-import grpc
-import virtualassistant_pb2
-import virtualassistant_pb2_grpc
+import requests
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = virtualassistant_pb2_grpc.VirtualAssistantStub(channel)
+def main():
+    # URL do servidor
+    url = "http://localhost:5000/message"
 
-while True:
-    message_text = input("Digite a sua mensagem: ")
-    message = virtualassistant_pb2.VirtualAssistantRequest(message=message_text)
-    response = stub.SendMessage(message)
-    print(response.message)
+    # Mensagem para enviar
+    continuar = True
+    while continuar: 
+        message = str(input(">>"))
+        if message == "":
+            continuar = False
+            break
+        data = {"message": message}     # Dados da mensagem em formato JSON
+
+        # Faz a requisição HTTP POST com a mensagem em formato JSON
+        response = requests.post(url, json=data)
+
+        # Verifica se a requisição foi bem sucedida (status code 200)
+        if response.status_code == 200:
+            # Exibe a mensagem de resposta do assistente virtual
+            print(response.json()["message"])
+        else:
+            print("Erro ao enviar mensagem. Status code:", response.status_code)
+
+if __name__ == '__main__':
+    main()
